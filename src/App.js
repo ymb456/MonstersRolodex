53bd44b28dc68b2dbcd4fdf53d64ee1cf3306949
YEV\YevLocal <yev.berezovsky@gmail.com>
2023-12-26 12:59:47 -0500

    SetState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  constructor (){
+    super();
+    this.state = {name:'Yev'};
+  }
+  render (){
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {this.state.name}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={()=>{this.setState({name:'Mila'})}}>
+        Change Name
+        </button>
+          
       </header>
     </div>
   );
+  }
 }
 
 export default App;
