07a21b9917a2eb802cbd5688e88c5340b13fe55b
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-03 10:32:04 -0500

    Async SetState demo : console.log added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,33 @@
-import {Component} from 'react'
+import { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
-  constructor (){
+  constructor() {
     super();
-    this.state = {name:{firstName: 'Y', lastName:'B'}, company:'Z'};
+    this.state = { name: { firstName: 'Y', lastName: 'B' }, company: 'Z' };
   }
-  render (){
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello {this.state.name.firstName} {this.state.name.lastName}. I work at {this.state.company}
-        </p>
-        <button onClick={()=>{this.setState({name:{firstName:'L', lastName:'B'}})}}>
-        Change Name
-        </button>
-          
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello {this.state.name.firstName} {this.state.name.lastName}. I work at {this.state.company}
+          </p>
+          <button onClick={() => {
+            this.setState(
+              { name: { firstName: 'L', lastName: 'B' } 
+            });
+            console.log (this.state);
+          }}>
+            Change Name
+          </button>
+
+        </header>
+      </div>
+    );
   }
 }
 
