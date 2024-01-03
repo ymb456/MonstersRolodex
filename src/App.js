9e76918d65bd11b43b258edeeca3691b9002dbc8
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-03 10:50:26 -0500

    Cleaning the App completely. Preparing for actual Monsters app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import { Component } from 'react'
-import logo from './logo.svg';
 import './App.css';
 
 
 class App extends Component {
   constructor() {
     super();
-    this.state = { name: { firstName: 'Y', lastName: 'B' }, company: 'Z' };
+    this.state = {};
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello {this.state.name.firstName} {this.state.name.lastName}. I work at {this.state.company}
-          </p>
-          <button onClick={() => {
-            this.setState((state, props) => {
-              return { name: { firstName: 'L', lastName: 'B' } }
-            },
-            ()=>{
-              console.log(this.state);
-            });
-          }}>
-            Change Name
-          </button>
-
-        </header>
       </div>
     );
   }
