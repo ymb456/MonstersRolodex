d14bdd78cd8c72fe3c79ea9c02e69c17eedabe64
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-03 12:28:36 -0500

    Step3: Add Search String to filter monsters

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
-import { Component } from 'react'
-import './App.css';
-
+import { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      monsters: []
+      monsters: [],
     };
   }
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then((response)=>response.json())
-    .then((users)=>{
-      this.setState(()=>{
-        return {monsters:users};
-      },
-      ()=>{
-        console.log(this.state)
-      })
-    });
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      });
   }
   render() {
     return (
       <div className="App">
-        {this.state.monsters.map ((monster)=>{
-          return <div key={monster.id}><h1>{monster.name}</h1></div>
-        })
-        }
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonsters = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+            this.setState({ monsters: filteredMonsters });
+          }}
+        />
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
