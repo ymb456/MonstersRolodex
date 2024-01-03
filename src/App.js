607965d95d2f948d9d9287c8ebdd7ee51ec81fc7
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-03 10:53:46 -0500

    Step1: Basic Monsters list implementation

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import './App.css';
 class App extends Component {
   constructor() {
     super();
-    this.state = {};
+    this.state = {
+      monsters: [
+        {name:'Linda'},
+        {name:'Frank'},
+        {name:'Jacky'}
+      ]
+    };
   }
   render() {
     return (
       <div className="App">
+        {this.state.monsters.map ((monster)=>{
+          return <h1>{monster.name}</h1>
+        })
+        }
       </div>
     );
   }
