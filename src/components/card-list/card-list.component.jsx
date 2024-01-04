745a108808f0ffe8685dc67a008aa874b2274336
YEV\YevLocal <yev.berezovsky@gmail.com>
2024-01-04 09:25:56 -0500

    STEP8: renaming styles files. Creating Card component. Adding google fonts. Finishing.

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
-import "./card-list.component.css";
+import "./card-list.styles.css";
+import Card from "../card/card.component";
 
 class CardList extends Component {
   render() {
@@ -7,17 +8,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return (
-            <div className="card-container" key={id}>
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/{id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} key={monster.id}></Card>;
         })}
       </div>
     );
